af1a65b2e3715625d041bc18a8d5e5067f33bcb8
vararais <varadigmaisnann@gmail.com>
2024-06-13 18:27:41 +0700

    Perbaikan Typo

--- BinarySearchTree_0049/BinarySearchTree_0049.cpp
+++ BinarySearchTree_0049/BinarySearchTree_0049.cpp
@@ -9,10 +9,30 @@ public:
     Node* leftchild;
     Node* rightchild;
 
-    Node(string i, Node * 1, Node* r) {
+    Node(string i, Node * l, Node* r) {
         info = i;
         leftchild = l;
         rightchild = r;
+    }
+};
+
+class BinaryTree {
+public:
+    Node* ROOT;
+
+    BinaryTree() {
+        ROOT = NULL;
+    }
+
+    void insert(string element) {
+        Node* newNode = new Node(element, NULL, NULL);
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node* parent = NULL;
+        Node* currentNode = NULL;
+        search(element, parent, currentNode);
     }
 };
 int main()
