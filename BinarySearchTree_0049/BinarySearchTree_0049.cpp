cdcae6abba9dff76a19408de6c4f7c5c2ab6815f
vararais <varadigmaisnann@gmail.com>
2024-06-13 19:47:21 +0700

    Menambahkan Case

--- BinarySearchTree_0049/BinarySearchTree_0049.cpp
+++ BinarySearchTree_0049/BinarySearchTree_0049.cpp
@@ -121,6 +121,41 @@ int main()
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter & word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+            x.preorder(x.ROOT);
+        }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+        }
+        case '5':
+        {
+            return 0;
+        default:
+        {
+            cout << "Invalid option" << endl;
+        }
     }
 }
 
