22ac151a5a10cb129cb615ddb58c4da0b86c26ec
vararais <varadigmaisnann@gmail.com>
2024-06-13 18:56:29 +0700

    Membuat Void Inorder dan Preorder

--- BinarySearchTree_0049/BinarySearchTree_0049.cpp
+++ BinarySearchTree_0049/BinarySearchTree_0049.cpp
@@ -62,6 +62,36 @@ public:
         }
       
     }
+
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+            
+        }
+    }
+
+    void preorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
 };
 int main()
 {
